0b8e1cea0c9ee86a5097de5884e2109af370f204
ktotheH <kcjhill1234@msn.com>
2020-06-12 11:33:40 -0600

    cont

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => res.send("test"));
+
+app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
