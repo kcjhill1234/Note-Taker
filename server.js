41aff98cf9429f3cbf2e34ae316883b04567587e
ktotheH <kcjhill1234@msn.com>
2020-06-17 13:19:34 -0600

    get all change

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 
 app.get("/notes", (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'notes.html')));
 
@@ -41,5 +40,6 @@ app.delete("/api/notes/:id", async (req, res) => {
     res.send('deleted');
 
 });
+app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
